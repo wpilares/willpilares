e5b29a70d4547f15b0b5ed50844c461a84cc73fa
wpilares <wgpilaresc@gmail.com>
2023-04-22 23:37:49 -0500

    added directory structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,18 @@
-import { type ReactElement, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../../vite.svg';
-import './App.css';
+import { type ReactElement } from 'react';
+import Header from './components/header/Header.tsx';
+import Hero from './content/hero/Hero.tsx';
+import Experience from './content/experience/Experience.tsx';
+import Contact from './content/contact/Contact.tsx';
+import Footer from './components/footer/Footer.tsx';
 
 function App(): ReactElement {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Hero />
+      <Experience />
+      <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/content/hero/Hero.tsx
@@ -0,0 +1,31 @@
+import { type ReactElement } from 'react';
+import styles from './Hero.module.scss';
+import profilePhoto from '../../assets/iron-man.jpg';
+import CV from '../../assets/CV_Willber_Pilares.pdf';
+
+const Hero = (): ReactElement => {
+  return (
+    <div className={styles.hero}>
+      <img src={profilePhoto} alt="foto" className={styles.photo} />
+      <div className={styles.about}>
+        <div className={styles.title}>Full Stack Developer</div>
+        <div className={styles.subtitle}>WILLBER PILARES</div>
+        <div className={styles.resume}>
+          I’m passionate about creating and learning about technology, with high
+          capacities in analysis and solutions, a responsible person with
+          initiative, creativity and punctuality, I gladly assume the challenges
+          and goals that may arise, with ease of teamwork and management of
+          interpersonal relationships, with the focus on achieving results.
+        </div>
+        <div className={styles.buttonSet}>
+          <a href={CV} download>
+            <button className={styles.button1}>Resume</button>
+          </a>
+          <button className={styles.button2}>Contact</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
